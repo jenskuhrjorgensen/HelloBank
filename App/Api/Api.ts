9aefaf41a3d53c6f3de07d5085f6e066041c15d3
Jens Kuhr Jørgensen <jens@journl.dk>
2020-07-13 14:27:49 +0200

    Add correct "results" prop to mocked normalized data in API.

--- App/Api/Api.ts
+++ App/Api/Api.ts
@@ -17,97 +17,96 @@ interface AccountsNormalized {
 export function fetchAccounts(): Promise<AccountsNormalized> {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            const accounts = {
+                "0": new AccountBuilder()
+                    .id("0")
+                    .name("Savings")
+                    .description("This account is for my savings")
+                    .owner("0")
+                    .build(),
+                "1": new AccountBuilder()
+                    .id("1")
+                    .name("Check")
+                    .description("This is my check account")
+                    .owner("0")
+                    .build(),
+                "2": new AccountBuilder()
+                    .id("2")
+                    .name("Real estate")
+                    .description("This is for my real estate expenses")
+                    .owner("1")
+                    .build(),
+                "3": new AccountBuilder()
+                    .id("3")
+                    .name("Private")
+                    .description("This is for fun!!")
+                    .owner("1")
+                    .build(),
+                "4": new AccountBuilder()
+                    .id("4")
+                    .name("Shared")
+                    .description("This is for my wife...")
+                    .owner("1")
+                    .build(),
+                "5": new AccountBuilder()
+                    .id("5")
+                    .name("Kids")
+                    .description("This is our kids' savings account")
+                    .owner("1")
+                    .build(),
+                "6": new AccountBuilder()
+                    .id("6")
+                    .name("Spending")
+                    .description("This is for general expenses")
+                    .owner("1")
+                    .build(),
+                "7": new AccountBuilder()
+                    .id("7")
+                    .name("Some other account 1")
+                    .description("Description")
+                    .owner("1")
+                    .build(),
+                "8": new AccountBuilder()
+                    .id("8")
+                    .name("Some other account 2")
+                    .description("Description")
+                    .owner("1")
+                    .build(),
+                "9": new AccountBuilder()
+                    .id("9")
+                    .name("Some other account 3")
+                    .description("Description")
+                    .owner("1")
+                    .build(),
+                "10": new AccountBuilder()
+                    .id("10")
+                    .name("Some other account 4")
+                    .description("Description")
+                    .owner("1")
+                    .build(),
+                "11": new AccountBuilder()
+                    .id("11")
+                    .name("Some other account 5")
+                    .description("Description")
+                    .owner("1")
+                    .build(),
+                "12": new AccountBuilder()
+                    .id("12")
+                    .name("Some other account 6")
+                    .description("Description")
+                    .owner("1")
+                    .build(),
+                "13": new AccountBuilder()
+                    .id("13")
+                    .name("Some other account 7")
+                    .description("Description")
+                    .owner("1")
+                    .build(),
+            }
             resolve({
-                result: [
-                    "0",
-                ],
+                result: Object.keys(accounts),
                 entities: {
-                    accounts: {
-                        "0": new AccountBuilder()
-                            .id("0")
-                            .name("Savings")
-                            .description("This account is for my savings")
-                            .owner("0")
-                            .build(),
-                        "1": new AccountBuilder()
-                            .id("1")
-                            .name("Check")
-                            .description("This is my check account")
-                            .owner("0")
-                            .build(),
-                        "2": new AccountBuilder()
-                            .id("2")
-                            .name("Real estate")
-                            .description("This is for my real estate expenses")
-                            .owner("1")
-                            .build(),
-                        "3": new AccountBuilder()
-                            .id("3")
-                            .name("Private")
-                            .description("This is for fun!!")
-                            .owner("1")
-                            .build(),
-                        "4": new AccountBuilder()
-                            .id("4")
-                            .name("Shared")
-                            .description("This is for my wife...")
-                            .owner("1")
-                            .build(),
-                        "5": new AccountBuilder()
-                            .id("5")
-                            .name("Kids")
-                            .description("This is our kids' savings account")
-                            .owner("1")
-                            .build(),
-                        "6": new AccountBuilder()
-                            .id("6")
-                            .name("Spending")
-                            .description("This is for general expenses")
-                            .owner("1")
-                            .build(),
-                        "7": new AccountBuilder()
-                            .id("7")
-                            .name("Some other account 1")
-                            .description("Description")
-                            .owner("1")
-                            .build(),
-                        "8": new AccountBuilder()
-                            .id("8")
-                            .name("Some other account 2")
-                            .description("Description")
-                            .owner("1")
-                            .build(),
-                        "9": new AccountBuilder()
-                            .id("9")
-                            .name("Some other account 3")
-                            .description("Description")
-                            .owner("1")
-                            .build(),
-                        "10": new AccountBuilder()
-                            .id("10")
-                            .name("Some other account 4")
-                            .description("Description")
-                            .owner("1")
-                            .build(),
-                        "11": new AccountBuilder()
-                            .id("11")
-                            .name("Some other account 5")
-                            .description("Description")
-                            .owner("1")
-                            .build(),
-                        "12": new AccountBuilder()
-                            .id("12")
-                            .name("Some other account 6")
-                            .description("Description")
-                            .owner("1")
-                            .build(),
-                        "13": new AccountBuilder()
-                            .id("13")
-                            .name("Some other account 7")
-                            .description("Description")
-                            .owner("1")
-                            .build(),
-                    },
+                    accounts: accounts,
                     owners: {
                         "0": new OwnerBuilder().firstName("John").lastName("Doe").build(),
                         "1": new OwnerBuilder().firstName("Paul").lastName("Rudd").build(),
--- App/Redux/Actions/AccountActions.ts
+++ App/Redux/Actions/AccountActions.ts
@@ -1,6 +1,5 @@
 import {
     AccountActions,
-    AccountsFilterClearAction,
     AccountsFilterSetAction,
     AccountsSetAction,
     AccountsSetPendingSetAction
